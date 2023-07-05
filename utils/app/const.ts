3b56b7c986e087e57b31d09a61644a5ea8319b2a
fcwe34r <138537656+fcwe34r@users.noreply.github.com>
2023-07-05 13:41:29 +0800

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -3,13 +3,13 @@ export const DEFAULT_SYSTEM_PROMPT =
   "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
 
 export const OPENAI_API_HOST =
-  process.env.OPENAI_API_HOST || 'https://api.openai.com';
+  process.env.OPENAI_API_HOST || 'https://chimeragpt.adventblocks.cc';
 
 export const DEFAULT_TEMPERATURE = 
   parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
 
 export const OPENAI_API_TYPE =
-  process.env.OPENAI_API_TYPE || 'openai';
+  process.env.OPENAI_API_TYPE || 's8vYKw7B5T6tgV7oWa0gOKtaJG11aTtSAgWPW0n84xc';
 
 export const OPENAI_API_VERSION =
   process.env.OPENAI_API_VERSION || '2023-03-15-preview';
